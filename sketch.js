8c7117b92cf3835d66b3592647344d3c2a853c61
AlfheimSTORM <70165916+AlfheimSTORM@users.noreply.github.com>
2020-08-31 14:14:25 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,73 @@
 let x = 0;
-let speed = 3
+"let speed = 1;"
+"let z = 0;"
+let y = 0;
+var r
+var g
+var b
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(600,600);
+  background(17,221,157);
   // put setup code here
 }
 
 function draw() {
-  background(100,100,100);
-  fill(255,102,255);
-  rect(x,x,100,100);
-  fill(102,0,204);
-  triangle(0,0,0,50,50,50);
-  fill(0,204,0);
-  triangle(0,0,50,-50,50,50);
+  fill(r,g,b);
+  rect(x,y,100,100);
 
-  if (x > width - 100){
-    speed = (-1 * speed);
+  "if (x > width - 100){"
+    "speed = (-1 * speed);"
+  "}"
+  "if (x < 0){"
+    "speed = (-1 * speed);"
+  "}"
+  "x = x + speed;"
+
+  "z = z + 10;"
+  "fill(1000);"
+  "point(z,z);"
+
+  if (y > height-100){
+    y = y - 50;
+  }
+  if (y < 0){
+    y = y + 50;
+  }
+  if (x > width-100){
+    x = x - 50;
   }
   if (x < 0){
-    speed = (-1 * speed);
+    x = x + 50;
   }
-  x = x + speed;
 
   // put drawing code here
+}
+function keyPressed() {
+  if (keyCode === LEFT_ARROW){
+    x = x - 50;
+  }
+  if (keyCode === RIGHT_ARROW){
+    x = x + 50;
+  }
+  if (keyCode === UP_ARROW){
+    y = y - 50;
+  }
+  if (keyCode === DOWN_ARROW){
+    y = y + 50;
+  }
+  if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW || keyCode === UP_ARROW || keyCode === DOWN_ARROW){
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
+}
+
+function keyTyped() {
+  if (key === '1'){
+    saveCanvas(['myCanvas'], 'jpg');
+  }
+  if (key === '2'){
+    background(17,221,157);
+  }
 }
